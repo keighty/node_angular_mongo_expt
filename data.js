3efe425e0b45df79db2a8cc4db313acc54e81772
Katie Leonard <kleonard@newrelic.com>
2014-10-06 08:15:58 -0700

    Basic node server.

---
+++ data.js
@@ -0,0 +1,37 @@
+[
+  {
+    "favoriteFruit": "banana",
+    "lastName": "Kim",
+    "firstName": "Arlene"
+  },
+  {
+    "favoriteFruit": "apple",
+    "lastName": "Romero",
+    "firstName": "Gillespie"
+  },
+  {
+    "favoriteFruit": "apple",
+    "lastName": "Roberson",
+    "firstName": "Moreno"
+  },
+  {
+    "favoriteFruit": "apple",
+    "lastName": "Jimenez",
+    "firstName": "Deborah"
+  },
+  {
+    "favoriteFruit": "apple",
+    "lastName": "Rodgers",
+    "firstName": "Downs"
+  },
+  {
+    "favoriteFruit": "apple",
+    "lastName": "Giles",
+    "firstName": "Forbes"
+  },
+  {
+    "favoriteFruit": "strawberry",
+    "lastName": "Boyle",
+    "firstName": "Cornelia"
+  }
+]
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/simple');
+
+var personSchema = {
+  firstName: String,
+  lastName: String,
+  favouriteFruit: String
+}
+
+var Person = mongoose.model('Person', personSchema, 'people');
+
+// serve a collection
+var app = express();
+app.get('/', function(req, res) {
+  Person.find(function(err, doc) {
+    res.send(doc);
+  });
+});
+
+// serve a static page
+app.use('/hello', express.static(__dirname + '/hello'));
+
+app.listen(3030);
